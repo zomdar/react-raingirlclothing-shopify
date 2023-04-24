97b0d6a8ecbf9cbb685685901c90112631f5bb4e
zomdar <zomdar@gmail.com>
2023-04-24 14:32:15 -0400

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+// pages/index.tsx
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </main>
+    </div>
+  );
+}
